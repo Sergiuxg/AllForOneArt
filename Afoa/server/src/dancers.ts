fa08b6aaa569b13a8a4e634c817e96a4e5ee0d17
Sergiu <gorceagsergiu7@gmail.com>
2026-05-06 18:11:46 +0300

    max edited

--- Afoa/server/src/dancers.ts
+++ Afoa/server/src/dancers.ts
@@ -9,7 +9,7 @@ export const dancers = [
     "Nicu Garbuz",
     "Nicolae Sirghi",
     "Ștefan Secrieru",
-    "Maxim Musteată",
+    "Maxim Musteață",
     "Adriana Munteanu",
     "Daniela Spînu",
     "Carolina Tverdohleb",
